503fdd24d4923848e473f2b49b3e1e29838ddcd9
FelixLeChat <felixlrc21@gmail.com>
2016-09-17 16:42:49 -0400

    starting google api

---
+++ MeetMeThere/MeetMeThere.MVC/Google/Google.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeetMeThere.MVC.Google
+{
+    public class Google
+    {
+        private string ApiKey = "AIzaSyAv_U9TbO9WMBlBPx3aWvJJGyhwUFWbGE0";
+
+    }
+}
