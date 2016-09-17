bc523449740301283193597f2d86eb86833c5d1e
FelixLeChat <felixlrc21@gmail.com>
2016-09-17 18:56:29 -0400

    Adding search results

---
+++ MeetMeThere/MeetMeThere.MVC/Service/SearchService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeetMeThere.MVC.Service
+{
+    public class SearchService
+    {
+        public string Name { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public string Type { get; set; }
+        public int Rating { get; set; }
+    }
+}
